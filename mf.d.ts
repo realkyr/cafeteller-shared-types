b6997a953ca0fa53c1a0b7de2ea53fde95fc9768
realkyr <phuree.kanusont@gmail.com>
2024-08-27 02:17:44 +0700

    feat(shared-types): init sub module shared-types

---
+++ mf.d.ts
@@ -0,0 +1,30 @@
+declare module 'core_cafeteller/components' {
+  import {
+    AddCardProps,
+    ButtonProps,
+    CardProps,
+    EditorProps,
+    FloatingButtonProps,
+    ImageProps,
+    SegmentControlProps
+  } from './components'
+
+  export const Card: React.FC<CardProps>
+  export const AddCard: React.FC<AddCardProps>
+  export const CoffeeLoader: React.FC
+  export const LazyImage: React.FC<ImageProps>
+  export const Button: React.FC<ButtonProps>
+  export const Editor: React.FC<EditorProps>
+  export const FloatingButton: React.FC<FloatingButtonProps>
+  export const SegmentControl: React.FC<SegmentControlProps>
+}
+
+declare module 'core_cafeteller/icons' {
+  import { CommonProps } from './index'
+
+  export const ReviewFace: React.FC<CommonProps>
+  export const Search: React.FC<CommonProps>
+  export const Instagram: React.FC<CommonProps>
+  export const Facebook: React.FC<CommonProps>
+  export const Twitter: React.FC<CommonProps>
+}
